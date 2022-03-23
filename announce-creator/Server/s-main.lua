176c8b710becdb946e38d24587b4362177ff6ccb
BK ^^ <84545556+Barikeloo@users.noreply.github.com>
2022-03-23 14:58:28 +0100

    added discord webhooks

--- announce-creator/Server/s-main.lua
+++ announce-creator/Server/s-main.lua
@@ -1,6 +1,27 @@
 ESX = nil
+Config = {}
+Config.Webhook = ""
+Config.Avatar = ""
+Config.Colour = 345001
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterNetEvent('announce:send', function(data)
+    SendLog("New annoucement", "Name: " .. GetPlayerName(source) .. "\nMessage: " .. data.msg)
     TriggerClientEvent('announce:sendForAll', -1, data)
 end)
+
+
+function SendLog(title, message)
+    local embedMsg = {}
+    embedMsg = {
+        {
+            ["color"] = Config.Colour,
+            ["title"] = title,
+            ["description"] =  ""..message.."",
+            ["footer"] ={
+                ["text"] = os.date("%c"),
+            },
+        }
+    }
+    PerformHttpRequest(Config.Webhook, function(err, text, headers)end, 'POST', json.encode({username = "Announce-creator", avatar_url= Config.Avatar ,embeds = embedMsg}), { ['Content-Type']= 'application/json' })
+end
